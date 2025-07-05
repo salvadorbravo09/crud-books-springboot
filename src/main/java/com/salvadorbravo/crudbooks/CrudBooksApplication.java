a7cf1a2244b7a635ca4e67ad0abb8e4d89ccebae
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-04 23:26:53 -0400

    first commit

---
+++ src/main/java/com/salvadorbravo/crudbooks/CrudBooksApplication.java
@@ -0,0 +1,13 @@
+package com.salvadorbravo.crudbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salvadorbravo/crudbooks/CrudBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvadorbravo.crudbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
