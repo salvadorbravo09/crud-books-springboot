a6f536477ec930867c760c77c41a3ebe14a845ff
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-05 23:41:47 -0400

    Add BookRepositoryImpl class for custom data access implementation

---
+++ src/main/java/com/salvadorbravo/crudbooks/repository/BookRepositoryImpl.java
@@ -0,0 +1,15 @@
+package com.salvadorbravo.crudbooks.repository;
+
+import com.salvadorbravo.crudbooks.model.Book;
+import org.springframework.stereotype.Repository;
+
+// @Repository
+public class BookRepositoryImpl { //implements BookRepository{
+
+    /*
+    @Override
+    public Book getBookById(int idBook) {
+        return new Book(idBook, "Effective Java", true);
+    }
+     */
+}
